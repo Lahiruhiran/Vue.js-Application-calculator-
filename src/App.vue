5fcd9374eac830eec4079572531b6424c412084a
unknown <lahiruhiranwijesooriyansbm@gmail.com>
2020-01-23 01:45:56 +0530

    strat making the calculater in vue.js

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <calculater/>
+  </div>
+</template>
+
+<script>
+import calculater from './components/calculater.vue'
+
+export default {
+ name: 'app',
+ components :{
+
+   calculater
+ }
+  }
+
+</script>
+
+<style>
+
+
+</style>
---
+++ src/components/calculater.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class ="calculator">
+<span>Hello</span>
+<span>Hello</span>
+<span>Hello</span>
+<span>Hello</span>
+<span>Hello</span>
+<span>Hello</span>
+<span>Hello</span>
+<span>Hello</span>
+    
+     
+  </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.calculator{
+  display: grid;
+  grid-template-columns :repeat(4,1fr);
+  grid-auto-rows: minmax(50px,auto);
+}
+</style>
